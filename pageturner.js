ae659410171db2afd16847228972c73212ae0aa5
Thomas Hart <myrridin@gmail.com>
2014-11-07 23:45:54 -0500

    Basic reading system and controls

--- pageturner.js
+++ pageturner.js
@@ -1,77 +1,146 @@
 var api_endpoint = "http://www.thomashart.me:4730/";
 var sentence_list;
 var voiceArr;
+var sentence_index = 0;
+var playing = false;
 
 $(document).ready(function() {
-
-  // Bindings
-  $('#load_book').click(load_book);
-  $('#button_play').click(play_book);
-
-  // Get voice array
-  voiceArr = speechSynthesis.getVoices();
-
+  pageturner_init();
+  bind_player_controls();
+  spanify_text_to_read();
 });
 
-function load_book(e) {
-  var book_id = parseInt($('#book_id').val());
-  if(is_int(book_id)) {
-    console.log("LOAD BOOK " + book_id);
-    $.get(api_endpoint + 'books/' + book_id, function(data) {
+function pageturner_init() {
+  // Get voice array
+  voiceArr = window.speechSynthesis.getVoices();
+}
 
-      console.log(data);
+function bind_player_controls() {
+  $('#btn_fast_backward').click(pageturner_fast_backward);
+  $('#btn_backward').click(pageturner_backward);
+  $('#btn_play').click(pageturner_play);
+  $('#btn_pause').click(pageturner_pause);
+  $('#btn_forward').click(pageturner_forward);
+}
+function pageturner_fast_backward(e) {
+  console.log('BUTTON fast backward');
+  sentence_index = 0;
+  stop_playing();
+  $('#btn_pause').hide();
+  $('#btn_play').show();
+  prevent_default(e);
+}
+function pageturner_backward(e) {
+  console.log('BUTTON backward');
 
-      var body = data.body;
-
-      // Split the body text on punctuation and line breaks
-      body = body.replace(/<p>/g, '<p><span>');
-      body = body.replace(/<\/p>/g, '</span></p>');
-      body = body.replace(/\./g, '</span>.<span>');
-      body = body.replace(/,/g, '</span>,<span>');
-      body = body.replace(/\!/g, '</span>!<span>');
-      body = body.replace(/\?/g, '</span>?<span>');
-      body = body.replace(/:/g, '</span>:<span>');
-      body = body.replace(/<i>/g, '');
-      body = body.replace(/<\/i>/g, '');
-
-      console.log(body);
-      $('#text_to_read').html(body);
-    });
+  if(sentence_index > 1) {
+    sentence_index -= 2;
+    if(window.speechSynthesis.speaking) {
+      window.speechSynthesis.cancel();
+    }
   }
-  e.preventDefault();
+  prevent_default(e);
+}
+function pageturner_play(e) {
+  console.log('BUTTON play');
+  $('#btn_pause').show();
+  $('#btn_play').hide();
+  play_sentence();
+  prevent_default(e);
+}
+function pageturner_pause(e) {
+  console.log('BUTTON pause');
+  sentence_index -= 1;
+  $('#btn_pause').hide();
+  $('#btn_play').show();
+  stop_playing();
+  prevent_default(e);
+}
+function pageturner_forward(e) {
+  console.log('BUTTON forward');
+  if(window.speechSynthesis.speaking) {
+    window.speechSynthesis.cancel();
+  }
+  prevent_default(e);
 }
 
-function play_book(e) {
-  console.log("PLAY BOOK");
+function spanify_text_to_read() {
+  var text_div = $('#text_to_read');
+  var body_text = text_div.html();
+  body_text = body_text.replace(/<p>/g, '<p><span>');
+  body_text = body_text.replace(/<\/p>/g, '</span></p>');
+  body_text = body_text.replace(/\./g, '</span>.<span>');
+  body_text = body_text.replace(/,/g, '</span>,<span>');
+  body_text = body_text.replace(/\!/g, '</span>!<span>');
+  body_text = body_text.replace(/\?/g, '</span>?<span>');
+  body_text = body_text.replace(/:/g, '</span>:<span>');
+  body_text = body_text.replace(/<i>/g, '');
+  body_text = body_text.replace(/<\/i>/g, '');
+  body_text = body_text.replace(/<span>\s*<\/span>/g, ' ');
+  text_div.html(body_text);
   sentence_list = $('#text_to_read span');
-  play_sentence(0);
-  e.preventDefault();
 }
 
-function play_sentence(sentence_index) {
-  sentence_list.each(function() {
-    $(this).css('background-color', '');
-  });
+function play_sentence() {
+  playing = true;
+  clear_highlighting();
+
   if(sentence_index < sentence_list.length) {
     var sentence = sentence_list.eq(sentence_index);
     sentence.css('background-color', 'yellow');
     var speech_text = sentence.html();
-    var utterance = new SpeechSynthesisUtterance(speech_text);
-    utterance.voice = voiceArr[10];
-    window.speechSynthesis.speak(utterance);
-    setTimeout(function() { play_sentence_when_ready(sentence_index + 1) }, 50);
-  }
-}
-
-function play_sentence_when_ready(sentence_index) {
-  if(window.speechSynthesis.speaking) {
-    setTimeout(function() { play_sentence_when_ready(sentence_index) }, 50);
+    if(speech_text != '') {
+      var utterance = new SpeechSynthesisUtterance(speech_text);
+      utterance.voice = voiceArr[10];
+      utterance.rate = 0.1;
+      utterance.onend = function() {
+        if(playing && !window.speechSynthesis.speaking) {
+          play_sentence();
+        }
+      };
+      window.speechSynthesis.speak(utterance);
+      sentence_index += 1;
+      setTimeout(function () {
+        play_sentence_when_ready();
+      }, 50);
+    }
+    else {
+      sentence_index += 1;
+      play_sentence();
+    }
   }
   else {
-    play_sentence(sentence_index);
+    pageturner_fast_backward();
   }
 }
 
-function is_int(val) {
-  return typeof val == "number" && isFinite(val) && val % 1 === 0;
+function stop_playing() {
+  playing = false;
+  window.speechSynthesis.cancel();
+  clear_highlighting();
+}
+
+function play_sentence_when_ready() {
+  if(playing) {
+    if (window.speechSynthesis.speaking) {
+      setTimeout(function () {
+        play_sentence_when_ready()
+      }, 50);
+    }
+    else {
+      play_sentence();
+    }
+  }
+}
+
+function clear_highlighting() {
+  sentence_list.each(function() {
+    $(this).css('background-color', '');
+  });
+}
+
+function prevent_default(e) {
+  if(typeof e !== 'undefined') {
+    e.preventDefault();
+  }
 }
